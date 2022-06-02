529d6f6f3a00a5d48fd0a145e4c312527532a5d1
Nice Guy <tabvim.dev@gmail.com>
2022-06-02 15:47:00 +0800

    (feat). add image config

---
+++ utils/data.ts
@@ -0,0 +1,15 @@
+interface Image {
+  src: string[];
+  type: string;
+}
+
+/**
+ * 类型与图片后缀映射关系
+ */
+const IMAGE_MAP_WITH_TYPE = new Map([
+  ["tops", [8, 10, 12, 22]],
+  ["bottoms", [30]],
+  ["dresses", [28]],
+]);
+
+export { IMAGE_MAP_WITH_TYPE };
