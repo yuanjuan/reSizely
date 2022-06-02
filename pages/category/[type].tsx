79062210888fc6db69ee5f3d769befadb774e8f8
Nice Guy <tabvim.dev@gmail.com>
2022-06-02 15:55:01 +0800

    (feat). basic layout

--- pages/category/[type].tsx
+++ pages/category/[type].tsx
@@ -1,7 +1,36 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { IMAGE_MAP_WITH_TYPE } from "../../utils/data";
 
 export default () => {
-  const router = useRouter()
-  const {type} = router.query
-  return <p>type: {type}</p>
-}
+  const router = useRouter();
+  const { type } = router.query;
+
+  const [categoryImages, setCategoryImages] = useState([]);
+
+  useEffect(() => {
+    const images = IMAGE_MAP_WITH_TYPE.get(type as string);
+    console.log("images: ", images);
+    if (images) {
+      setCategoryImages(images);
+    }
+  }, [type]);
+
+  return (
+    <section className='mt-4'>
+      <h1>{type}</h1>
+      <div className='container'>
+        {categoryImages.map((image) => (
+          <Image
+            key={image}
+            width='200'
+            height='200'
+            src={`/template/${image}.jpg`}
+            alt='template'
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,10 +1,10 @@
 import Header from "../components/Header";
 
-export default function Layout({children}: any) {
+export default function Layout({ children }: any) {
   return (
-    <main className="container bg-white">
+    <main className='container bg-white'>
       <Header />
-      <section className="container bg-slate-100">{children}</section>
+      <section className='container bg-slate-100'>{children}</section>
     </main>
-  )
+  );
 }
