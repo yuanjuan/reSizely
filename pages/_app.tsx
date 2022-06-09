3df756f32af5acddee20e1f3beb2f5d104bf0348
Nice Guy <tabvim.dev@gmail.com>
2022-06-09 17:14:38 +0800

    (feat). add page progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from "./layout";
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <NextNProgress />
       <Component {...pageProps} />
     </Layout>
   )
