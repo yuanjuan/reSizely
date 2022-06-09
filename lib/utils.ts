f760caa2ddae7b16918b3db117cc84dff6b42b92
Nice Guy <tabvim.dev@gmail.com>
2022-06-09 22:50:58 +0800

    (feat). add util methods

---
+++ lib/utils.ts
@@ -0,0 +1,46 @@
+/**
+ * https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+ * @author Dean Taylor
+ * @param text
+ */
+
+// TODO: fail to notice
+
+function fallbackCopyTextToClipboard(text: any) {
+  let textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    let successful = document.execCommand("copy");
+    let msg = successful ? "successful" : "unsuccessful";
+    console.log("Fallback: Copying text command was " + msg);
+  } catch (err) {
+    console.error("Fallback: Oops, unable to copy", err);
+  }
+
+  document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text: any) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(
+    function () {
+      console.log("Async: Copying to clipboard was successful!");
+    },
+    function (err) {
+      console.error("Async: Could not copy text: ", err);
+    }
+  );
+}
+
+export { copyTextToClipboard };
